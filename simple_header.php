f0f3e3cdba931ff7df6ad077a6f80b369a4a79ed
Lugiafanatic <lugiafanatic@gmail.com>
2013-07-13 19:25:47 -0400

    Create simple_header.php

---
+++ simple_header.php
@@ -0,0 +1,7 @@
+<div id="header">
+<center>
+  <a href="parukia.host22.com/test.html"><img src="http://fc01.deviantart.net/fs70/f/2013/192/8/c/parukia_net_banner_by_xkuyu-d6d295i.png" border="0" 
+
+alt="Banner"></a>
+</center> 
+</div>
