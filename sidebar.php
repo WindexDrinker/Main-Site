c6e6ecf2d7370505109469ed837a0a85041813e3
Lugiafanatic <lugiafanatic@gmail.com>
2013-07-14 16:50:00 -0400

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,22 @@
 <div id="sidebar">
-Sidebar content goes here.
+<center>
+<table border="1" id="eventable">
+<tr>
+<td id="eventablepic"> <div id="nontran"><img src="http://img833.imageshack.us/img833/8061/apzf.png" alt="Meloetta" width="100%"></div>    </td>
+<td id="eventabletex"> <p id="evtext"><b>Pok�mon Name</b></p>    </td>
+</tr>
+</table>
+
+<img src="http://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/285px-Flag_of_the_United_States.svg.png" height="15%" width="60%" title="America" />
+<br />
+<b>Start/End Date:</b> March 4 - March 24
+<br />
+<b>Where can I get it?:</b> Gamestop
+<br />
+<b>What games is it for?:</b> BW & B2W2
+<br />
+<a href="blah">Other countries this event is taking place in!</a>
+<br />
+
 <p class="push"></p>
 </div>
