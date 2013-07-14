6a96a4ceea8280bc36e4ac87a589e3e1a827da10
Parukia <dragontrainerryan724@yahoo.com>
2013-07-13 18:25:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -19,30 +19,23 @@
 
 
 <div id="content">
-	<h1 id="tex">This is a title this is a title this is a title </h1>
-	<p id="par">This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is 
+	<h1 id="tex">Welcome to Parukia's main website!</h1>
+	<p id="par">We still have a LOT of work to do on it, but bear with us as we turn this into a masterpiece never before seen! In the meantine, please check out our forum and showdown! server:
+	<br>
+	<br>
+	Forum: <a href="http://forums.parukia.net/forum.php">http://www.forums.parukia.net/forum.php</a>
+	<br>
+	PS! Server: <a href="http://parukia.psim.us/">http://parukia.psim.us/</a>
+	<br>
+	<br>
+	Thanks for being patient with us: This website is scheduled to be done by the end of July 2013!
+	<br>
+	<br>
+	Sincerely,
+	<br>
+	<br>
+	Gary Oak, Lugiafanatic, Uzumaki Naruto, Windex Drinker (The Lead Developers) and all of Parukia's other programmers</p>
 
-a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph </p>
-<h1 id="tex">This is a title this is a title this is a title </h1>
-	<p id="par">This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is 
-
-a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph </p>
-<h1 id="tex">This is a title this is a title this is a title </h1>
-	<p id="par">This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is 
-
-a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph This is a 
-
-paragraph This is a paragraph This is a paragraph This is a paragraph This is a paragraph </p>
 </div>
 
 
