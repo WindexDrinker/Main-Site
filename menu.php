3c7354dbe139ebb518b818cbcaf6658f68107684
Lugiafanatic <lugiafanatic@gmail.com>
2013-07-14 14:34:59 -0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,44 @@
+<div id="menu">
+
+<nav>
+  <ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Forums</a></li>
+		<li><a href="#">About Us</a></li>
+		<li><a href="#">In-Game Guides</a>
+			<ul>
+				<li><a href="#">Red/Blue/Yellow</a></li>
+				<li><a href="#">Gold/Silver/Crystal</a></li>
+				<li><a href="#">Generation III</a>
+					<ul>
+						<li><a href="#">Ruby/Sapphire/Emerald</a></li>
+						<li><a href="#">FireRed/LeafGreen</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Generation IV</a>
+					<ul>
+						<li><a href="#">Diamond/Pearl/Platinum</a></li>
+						<li><a href="#">HeartGold/SoulSilver</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Generation V</a>
+					<ul>
+						<li><a href="#">Black/White</a></li>
+						<li><a href="#">Black2/White2</a></li>
+					</ul>
+				</li>
+				<li><a href="#">X/Y</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Competitive Guides</a>
+			<ul>
+				<li><a href="#">Pok�mon Strategy Guides</a></li>
+				<li><a href="#">Tiers</a></li>
+				<li><a href="#">Battle Simulator</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
+
+
+</div>
