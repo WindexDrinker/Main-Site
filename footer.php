2eac8726b85661a1f51a354a27caaa79d97802ec
Lugiafanatic <lugiafanatic@gmail.com>
2013-07-14 14:35:37 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<div class="push"></div>
+        </div>
+  <center>
+        <div class="footer">
+        <table border="0" id="ft">
+	<tr>
+	  <td id="ftex">
+            <p id="ftext"> <i>Copyright � 2012 � 2013 Parukia.Net. Pok�mon and Pok�mon character names belong to Nintendo, Creatures, GAME FREAK, and The Pok�mon Company International. Parukia.Net is not affiliated with Nintendo. All content on this site belongs to their respective authors. Click <a href="">here</a> to view our Credits page! Thanks to Chris Spooner of line25.com for the dropdown menu! </i></p>
+	  </td>
+	  <td>
+<table border="0" id="sn">
+<tr><td><a href="">deviantArt</a></td></tr>
+<tr><td><a href="">Twitter</a></td></tr>
+<tr><td><a href="">Facebook</a></td></tr>
+</tr>
+</table>
+</td>
+	</tr>
+	</table>
+        </div>
+	</center>
