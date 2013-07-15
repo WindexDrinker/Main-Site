143e5f6bcb44fa249bb574462efda1c29423f86a
WindexDrinker <robertotapia36@yahoo.com>
2013-07-14 17:17:15 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <table border="0" id="ft">
 	<tr>
 	  <td id="ftex">
-            <p id="ftext"> <i>Copyright � 2012 � 2013 Parukia.Net. Pok�mon and Pok�mon character names belong to Nintendo, Creatures, GAME FREAK, and The Pok�mon Company International. Parukia.Net is not affiliated with Nintendo. All content on this site belongs to their respective authors. Click <a href="">here</a> to view our Credits page! Thanks to Chris Spooner of line25.com for the dropdown menu! </i></p>
+            <p id="ftext"> <i>Copyright � 2012 � 2013 Parukia.Net. Pokemon and Pokemon Chararacter names belong to Nintendo, Creatures, GAME FREAK, and The Pokemon Company International. Parukia.Net is not affiliated with Nintendo. All content on this site belongs to their respective authors. Click <a href="">here</a> to view our Credits page! Thanks to Chris Spooner of line25.com for the dropdown menu! </i></p>
 	  </td>
 	  <td>
 <table border="0" id="sn">
